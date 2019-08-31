5d9cf0f79c2103edfc181b047cf3404df9ec19ae
jarekxyz <42453891+jarekxyz@users.noreply.github.com>
2019-08-31 22:02:00 +0200

    Created core app and added custom user model.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user(self):
+        """Test if creating a new user with email is successful"""
+        email = 'test@example.com'
+        password = 'P@ssword123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
